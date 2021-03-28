a5efc82398a33a125cbd4cb34de910f0d0225a85
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-03-28 12:29:26 -0300

    feat: add welcoming message

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const Discord = require('discord.js');
 require('config/dotenv');
 const token = process.env.TOKEN;
+const prefix = '!';
 
 const app = new Discord.Client();
 
 app.on('ready', () => {
     console.log('Bom dia, amigo!');
+});
+
+app.on("guildCreate", guild => {
+    let channelID;
+    let channels = guild.channels.cache;
+
+    channelLoop:
+    for (let key in channels) {
+        let c = channels[key];
+        if (c[1].type === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
+    const msg = new Discord.MessageEmbed()
+      .setTitle('BOM DIA, AMIGO!')
+			.setDescription('É, amigo. A complexidade chegou! Tô indo de base por enquanto, mas se quiser saber o que eu faço, manda um !help')
+			.setFooter('NÃO COMETA EUTANÁSIA AINDA')
+    channel.send(msg);
 });
 
 app.login(token);
