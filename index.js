810398a41e51c3923c2f4241c8df2537a400f272
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-03-28 13:48:40 -0300

    refactor: remove welcoming message

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ app.on('ready', () => {
     console.log('Bom dia, amigo!');
 });
 
-app.on("guildCreate", guild => {
-    let channelID;
-    let channels = guild.channels.cache;
-
-    channelLoop:
-    for (let key in channels) {
-        let c = channels[key];
-        if (c[1].type === "text") {
-            channelID = c[0];
-            break channelLoop;
-        }
-    }
-
-    let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
-    const msg = new Discord.MessageEmbed()
-      .setTitle('BOM DIA, AMIGO!')
-			.setDescription('É, amigo. A complexidade chegou! Tô indo de base por enquanto, mas se quiser saber o que eu faço, manda um !help')
-			.setFooter('NÃO COMETA EUTANÁSIA AINDA')
-    channel.send(msg);
-});
-
 app.on('message', (msg) => {
     if((msg.content === `${prefix}help` && msg.member.voice.channel && !msg.author.bot)) {
         const message = new Discord.MessageEmbed()
